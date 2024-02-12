ac7a552ce7a3fda0c190d3a1b8fe48a908e3600a
Rafael Salomão <rafaelsalomao212@gmail.com>
2024-02-12 10:41:43 -0300

    Adiciona suporte para entrada de minutos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel.Design.Serialization;
 using System.Security.Policy;
 
 namespace Stopwatch
@@ -7,38 +8,93 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
+
+            int seconds;
+            int minutes;
+
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Stopwatch".PadLeft(20 / 2 + "Stopwatch".Length / 2));
-                Console.WriteLine(new string('-', 20));
-                Console.Write("Set time [seconds]: ");
-                int seconds = int.Parse(Console.ReadLine());
-                Console.WriteLine("Press [Enter] to start.");
-                Console.ReadKey();
-                Start(seconds);
+                Menu();
+
+                string inputTime = Console.ReadLine();
+                char type = char.Parse(inputTime.Substring(inputTime.Length - 1, 1));
+
+                if (type == 's')
+                {
+                    seconds = int.Parse(inputTime.Substring(0, inputTime.Length - 1));
+                    StartSeconds(seconds);
+                }
+                else if (type == 'm')
+                {
+                    minutes = int.Parse(inputTime.Substring(0, inputTime.Length - 1));
+                    StartMinutes(minutes);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid data type.");
+                }
+
                 Again();
             }
         }
 
-        static void Start(int seconds)
+        static void Menu()
+        {
+            ClearScreen();
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("Stopwatch".PadLeft(20 / 2 + "Stopwatch".Length / 2));
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("Ex:  10m = 10 minutes");
+            Console.WriteLine("Ex: 100s = 100 seconds");
+            Console.Write("Set time: ");
+        }
+
+        static void StartSeconds(int seconds)
         {
             int hundredth;
 
             while (seconds != 0)
             {
+                seconds--;
                 hundredth = 100;
-                Console.Clear();
+                ClearScreen();
                 while (hundredth != 0)
                 {
-                    Console.Clear();
+                    ClearScreen();
                     hundredth--;
                     Console.WriteLine($"{seconds}:{hundredth}");
                     Thread.Sleep(1);
                 }
-                seconds--;
             }
-            Console.Clear();
+            ClearScreen();
+            Console.WriteLine("End...");
+        }
+
+        static void StartMinutes(int minutes)
+        {
+            int seconds = 0;
+            int hundredth = 0;
+
+            while (minutes != 0)
+            {
+                minutes--;
+                ClearScreen();
+                seconds = 60;
+                while (seconds != 0)
+                {
+                    seconds--;
+                    hundredth = 100;
+                    while (hundredth != 0)
+                    {
+                        ClearScreen();
+                        hundredth--;
+                        Console.WriteLine($"{minutes}:{seconds}:{hundredth}");
+                        Thread.Sleep(1);
+                    }
+                }
+
+            }
+            ClearScreen();
             Console.WriteLine("End...");
         }
 
@@ -52,6 +108,18 @@ namespace Stopwatch
             {
                 Console.WriteLine("Closing...");
                 Environment.Exit(0);
+            }
+        }
+    
+        static void ClearScreen()
+        {
+            try
+            {
+                Console.Clear();
+            }
+            catch (System.IO.IOException)
+            {
+                //ignore
             }
         }
     }
